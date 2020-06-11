5c810f76e698809983d6b4319acc677a08555439
Fernando Mercadal <fermercadal@outlook.com>
2020-06-11 10:12:30 -0300

    Update pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,20 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
-      <h2>
+      <h2>A functional component</h2>
+      <nav>
         <Link href="/">
           <a>Back to home</a>
         </Link>
-      </h2>
+      </nav>
     </>
   )
 }
